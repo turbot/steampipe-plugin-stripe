71b753aa600c0f5ca8f15afecac9c0f95d7a5023
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-28 15:25:21 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#9)

--- stripe/connection_config.go
+++ stripe/connection_config.go
@@ -1,8 +1,8 @@
 package stripe
 
 import (
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/schema"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/schema"
 )
 
 type stripeConfig struct {
--- stripe/table_stripe_subscription.go
+++ stripe/table_stripe_subscription.go
@@ -4,9 +4,9 @@ import (
 	"context"
 
 	"github.com/stripe/stripe-go"
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableStripeSubscription(ctx context.Context) *plugin.Table {
