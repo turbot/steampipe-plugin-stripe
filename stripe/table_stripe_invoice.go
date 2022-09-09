70852e192a6ee5e97f2a097a022deff07a2734ac
Ved misra <47312748+misraved@users.noreply.github.com>
2022-09-09 11:22:35 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19 (#16)

--- stripe/table_stripe_invoice.go
+++ stripe/table_stripe_invoice.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/stripe/stripe-go"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 func tableStripeInvoice(ctx context.Context) *plugin.Table {
